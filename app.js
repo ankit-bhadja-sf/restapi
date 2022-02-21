912df021b78f7fa82266a18378ea33eeb1f0057e
[ankit bhadaja] <[ankit.bhadaja@seaflux.tech]>
2022-02-21 19:23:09 +0530

    [feature] 386 user signup validation complete

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const  mongoose = require('mongoose');
+const multer = require('multer');
 
-const feedRoutes = require('./routes/feed')
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
 app.use(bodyParser.json());
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
+app.use('images', express.static(path.join(__dirname, 'images'))); 
 
 app.use( (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,8 +38,24 @@ app.use( (req, res, next) => {
     next()   
 })
 
-app.use('/feed', feedRoutes)
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080, () => {
-    console.log('You are now on 8080');
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({message: message, data: data });
+
 });
+app.listen(8080, () => {
+        console.log('You are now on 8080');
+    });
+mongoose.connect('mongodb://localhost:27017/React')
+    .then(result => {
+        
+    })
+    .catch( err => {
+        
+    })
+
--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const { body } = require('express-validator/check');
 
-const feedController = require('../controller/feed')
+const feedController = require('../controller/feed');
 
 const router = express.Router();
 
-//Get /feed /posts
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-//Get /feed /posts
-router.post('/posts', feedController.createPost);
+// POST /feed/post
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost
+);
+
+router.get('/post/:postId', feedController.getPost);
+
+router.put(
+  '/post/:postId',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.updatePost
+  );
+
+router.delete('/post/:postId', feedController.deletePost);
 
 module.exports = router;
